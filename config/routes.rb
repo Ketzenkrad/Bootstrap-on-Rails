ffa2a9f132664cee0c45dd43139fd3c85173c742
Ketzenkrad <ketzenkrad@gmail.com>
2018-11-07 20:09:25 +0300

    Controllers++

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Роут для контроллера
+  #указание имени контроллера и метода его реализации;
+  #Main page
+  root 'posts#index'
+  # Роут для страницы обо мне;
+  # get обработка иных страниц;
+  get 'about' => 'pages#about'
+  # Динамическое отслеживание;
+  # Контроллер постов;
+  resources :posts
 end
